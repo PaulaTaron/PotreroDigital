1039b8233758e3a9f3b16703a98eb994650a2c8b
PaulaTaron <116023349+PaulaTaron@users.noreply.github.com>
2022-11-07 16:52:02 -0300

    Trabajo practico clase 9

---
+++ js/javascript.js
@@ -0,0 +1,29 @@
+/* Funcion para cambiar la página a modo oscuro */
+function modoOscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.getElementById("contenedorNav").style.backgroundColor = "black";
+    document.getElementById("contenedorBoton").style.backgroundColor = "black";
+    document.getElementById("header").style.background = "none";
+    document.getElementById("footer").style.background = "none";
+    document.getElementById("header").style.backgroundColor = "whitesmoke";
+    document.getElementById("footer").style.backgroundColor = "whitesmoke";
+    document.getElementById("link1").style.color = "white";
+    document.getElementById("link2").style.color = "white";
+    document.getElementById("link3").style.color = "white";
+    document.getElementById("title").style.color = "white";
+    document.getElementById("sub1").style.color = "white";
+    document.getElementById("sub2").style.color = "white";
+    document.getElementById("sub3").style.color = "white";
+    document.getElementById("sub4").style.color = "white";
+    document.getElementById("sub5").style.color = "white";
+    document.getElementById("card1").style.color = "black";
+    document.getElementById("card2").style.color = "black";
+    document.getElementById("card3").style.color = "black";
+    document.getElementById("card4").style.color = "black";
+    }
+
+    /* Funcion que muestra los datos del creador */
+function mostrarDatos(){
+  alert ("Paula Taron- Potrero Digital 2022- taronpaula@gmail.com")
+}
