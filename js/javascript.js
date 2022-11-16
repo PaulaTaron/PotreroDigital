2396a469d0327e7f1f53534b95cc1b889dc298ce
PaulaTaron <116023349+PaulaTaron@users.noreply.github.com>
2022-11-16 00:38:52 -0300

    Cambios de estilo

--- js/javascript.js
+++ js/javascript.js
@@ -2,15 +2,11 @@
 function modoOscuro() {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
-    document.getElementById("contenedorNav").style.backgroundColor = "black";
     document.getElementById("contenedorBoton").style.backgroundColor = "black";
     document.getElementById("header").style.background = "none";
     document.getElementById("footer").style.background = "none";
     document.getElementById("header").style.backgroundColor = "whitesmoke";
     document.getElementById("footer").style.backgroundColor = "whitesmoke";
-    document.getElementById("link1").style.color = "white";
-    document.getElementById("link2").style.color = "white";
-    document.getElementById("link3").style.color = "white";
     document.getElementById("title").style.color = "white";
     document.getElementById("sub1").style.color = "white";
     document.getElementById("sub2").style.color = "white";
